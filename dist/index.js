722fca8b530d1dd8fe1138da7988b9c2662035cd
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-02-07 17:27:28 +0000

    ts arrays

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,27 @@ let firstName = "Mario";
 let isFictional;
 firstName = "Luigi";
 isFictional = true;
+//--------------------
 // type inference (ts auto assigns the types - they are stil non changeable)
+//--------------------
 let planet = "earth";
 let moons = 1;
 let isLarge = true;
 // planet = 10 (error)
 // planet = "saturn" (fine)
+//--------------------
+// explicit type arrays
+//--------------------
+let names = ["blake", "luigi", "mario"];
+let ages = [25, 28, 24];
+names.push("helen");
+ages.push(100);
+//--------------------
+// infered arrays (typescript will auto assign strings as the only type acceptable inside "fruits" array)
+//--------------------
+let fruits = ["apples", "mangos", "oranges"];
+//fruits.push(23) flags error
+//fruits.push("litchis") ✔️
+//--------------------
+// object literals
+//--------------------
