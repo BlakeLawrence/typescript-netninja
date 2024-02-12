5ecedeeebf2d6d303e151a89a39343456ec72da5
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-02-12 08:03:35 +0000

    quick practice

---
+++ src/practice.tsx
@@ -0,0 +1,12 @@
+let me: string = "blake";
+let numberFive: number = 5;
+let myList: number[] = [1, 2, 3, 4, 5];
+myList.push(5);
+
+let userTwo: { name: string; age: number; email: string };
+userTwo = { name: "Blake", age: 35, email: "blake@balke.com" };
+
+function wordConcat(item1: string, item2: string): string {
+  return item1 + item2;
+}
+wordConcat("blake", "lawrence");
